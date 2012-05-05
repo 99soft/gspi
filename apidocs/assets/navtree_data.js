9ea6187c3bccefb6d3fe4cbae9d1525915ff59e3
simonetripodi <simone.tripodi@gmail.com>
2012-05-05 23:27:49 +0200

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,9 @@
+var NAVTREE_DATA =
+[ [ "org.nnsoft.guice.gspi", "org/nnsoft/guice/gspi/package-summary.html", [ [ "Description", "org/nnsoft/guice/gspi/package-descr.html", null, "" ], [ "Classes", null, [ [ "GuiceServiceLoader", "org/nnsoft/guice/gspi/GuiceServiceLoader.html", null, "" ], [ "ServiceLoaderModule", "org/nnsoft/guice/gspi/ServiceLoaderModule.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.nnsoft.guice.gspi.binder", "org/nnsoft/guice/gspi/binder/package-summary.html", [ [ "Description", "org/nnsoft/guice/gspi/binder/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "AnnotatedServiceBuilder", "org/nnsoft/guice/gspi/binder/AnnotatedServiceBuilder.html", null, "" ], [ "FromClassLoaderBuilder", "org/nnsoft/guice/gspi/binder/FromClassLoaderBuilder.html", null, "" ], [ "ServiceBuilder", "org/nnsoft/guice/gspi/binder/ServiceBuilder.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,10 @@
+var DATA = [
+      { id:0, label:"org.nnsoft.guice.gspi", link:"org/nnsoft/guice/gspi/package-summary.html", type:"package" },
+      { id:1, label:"org.nnsoft.guice.gspi.GuiceServiceLoader", link:"org/nnsoft/guice/gspi/GuiceServiceLoader.html", type:"class" },
+      { id:2, label:"org.nnsoft.guice.gspi.ServiceLoaderModule", link:"org/nnsoft/guice/gspi/ServiceLoaderModule.html", type:"class" },
+      { id:3, label:"org.nnsoft.guice.gspi.binder", link:"org/nnsoft/guice/gspi/binder/package-summary.html", type:"package" },
+      { id:4, label:"org.nnsoft.guice.gspi.binder.AnnotatedServiceBuilder", link:"org/nnsoft/guice/gspi/binder/AnnotatedServiceBuilder.html", type:"class" },
+      { id:5, label:"org.nnsoft.guice.gspi.binder.FromClassLoaderBuilder", link:"org/nnsoft/guice/gspi/binder/FromClassLoaderBuilder.html", type:"class" },
+      { id:6, label:"org.nnsoft.guice.gspi.binder.ServiceBuilder", link:"org/nnsoft/guice/gspi/binder/ServiceBuilder.html", type:"class" }
+
+    ];
